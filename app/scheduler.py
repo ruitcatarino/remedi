aab2f32eed72e9865f6bf48f873b056070cbf6ef
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2025-05-29 12:01:09 +0100

    Implament queries of scheduler

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,7 +1,13 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 from apscheduler.jobstores.redis import RedisJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyttings import settings
+from tortoise.expressions import Q
 
+from app.models.medication import Medication
+from app.models.medication_schedule import MedicationSchedule, MedicationStatus
 from app.models.token_blacklist import BlacklistedToken
 
 
@@ -51,21 +57,45 @@ class Scheduler:
 
     async def generate_medication_reminders(self) -> None:
         """
-        Create medication schedules for the upcoming day.
+        Create medication schedules for the defined period.
         Skip if already created.
         """
-        ...  # TODO
+        now = datetime.now(ZoneInfo("UTC"))
+
+        future_meds: list[Medication] = await Medication.filter(
+            Q(start_date__gt=now, end_date__isnull=True, is_active=True, is_prn=False)
+            | Q(start_date__gt=now, end_date__gt=now, is_active=True, is_prn=False)
+        ).all()
+
+        for med in future_meds:
+            await med.generate_schedules()
 
     async def check_medication_schedules(self) -> None:
         """
         Check for medications schedules that where missed.
-        Not yet taken nor notified but in grace period.
+        Not yet taken nor notified.
         """
-        ...  # TODO
+        now = datetime.now(ZoneInfo("UTC"))
+
+        schedules: list[MedicationSchedule] = await MedicationSchedule.filter(
+            status=MedicationStatus.SCHEDULED, scheduled_datetime__lt=now
+        ).all()
+
+        for schedule in schedules:
+            await schedule.handle_medication_notification()
 
     async def handles_missed_medications(self) -> None:
         """Handles missed medications, bigger than grace period."""
-        ...  # TODO
+        now = datetime.now(ZoneInfo("UTC"))
+        grace_period = timedelta(minutes=settings.MISSED_GRACE_PERIOD)
+
+        missed_schedules: list[MedicationSchedule] = await MedicationSchedule.filter(
+            status=MedicationStatus.NOTIFIED,
+            scheduled_datetime__lt=now - grace_period,
+        ).all()
+
+        for schedule in missed_schedules:
+            await schedule.handle_missed_medication()
 
     def shutdown(self) -> None:
         self.scheduler.shutdown()
