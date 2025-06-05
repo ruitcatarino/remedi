c72eef35c5cd44c2c74f25866bdd5c447ce3fa2c
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2025-06-05 18:37:07 +0100

    Add phone verification

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,13 +1,14 @@
 from datetime import date
 
 from pydantic import BaseModel, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
 
 
 class UserSchema(BaseModel):
     email: EmailStr
     password: str
     name: str
-    phone_number: str
+    phone_number: PhoneNumber
     birth_date: date
 
 
